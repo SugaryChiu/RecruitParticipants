b4d324e5a9a6043ad5f4c18631ce9bb90049085d
Xiaoyang Liu <xliu68@jhu.edu>
2019-03-16 13:46:38 -0400

    IndexFromHtml

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const http = require('http');
+var fs = require('fs');
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Recruit Participants");
+    fs.readFile('index.html',function(err,data){
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(data);
+        response.end();    
+    });
 });
 
 const port = process.env.PORT || 1337;
